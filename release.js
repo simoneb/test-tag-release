1ed87ceb02e6788ddf44b233c6afbda1d2597e62
Simone Busoli <simone.busoli@gmail.com>
2021-12-04 14:18:57 +0100

    chore: new release script

--- release.js
+++ release.js
@@ -1,38 +1,61 @@
-module.exports = async function({ require, github, context }) {
-    const https = require('https')
+module.exports = notifyRelease
+module.exports.processReleaseBody = processReleaseBody
 
-    const { data: release } = await github.rest.repos.getReleaseByTag({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        tag: context.payload.release.tag_name
-    })
-    
-    return new Promise((resolve, reject) => {
-        const req = https.request(process.env.WEBHOOK_URL, {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        }, res => {
-            resolve(res.statusCode)
-        })
-        
-        req.write(JSON.stringify({
-            channel: '@simone',
-            attachments: [{
-                fallback: `New release ${release.tag_name}`,
-                pretext:   `New release ${release.tag_name}`,
-                fields: [{
-                    // title: '*{version}*',
-                    value: release.body
-                    .replace(/#+\s+(.+)/g, '*$1*')
-                    .replace(/\* /g, '\n- ')
-                    .replace(/\*+/g, '*'),
-                    short: false
-                }]
-            }]
-        }))
+function processReleaseBody(releaseBody) {
+  // basic markdown into slack syntax
+  return (
+    releaseBody
+      // headers
+      .replace(/#+\s+(.+)/g, '*$1*')
+      // dotted lists
+      .replace(/\* /g, '- ')
+      // bold
+      .replace(/\*+/g, '*')
+      // usernames
+      .replace(/@(.+)/g, '<https://github.com/$1|@$1>')
+      // github links
+      .replace(/https:\/\/github.com\/.+\/(?:pull|issue)\/(.+)/g, '<$&|#$1>')
+  )
+}
 
-        req.end()
-    })
+async function notifyRelease({ require, github, context }) {
+  const https = require('https')
+
+  const { data: release } = await github.rest.repos.getReleaseByTag({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    tag: context.payload.release.tag_name
+  })
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(
+      process.env.SLACK_WEBHOOK_URL,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      res => (res.statusCode === 200 ? resolve : reject)(res.statusMessage)
+    )
+
+    req.write(
+      JSON.stringify({
+        attachments: [
+          {
+            fallback: `<${release.html_url}|New release ${release.tag_name}>`,
+            pretext: `<${release.html_url}|New release ${release.tag_name}>`,
+            fields: [
+              {
+                value: processReleaseBody(release.body),
+                short: false
+              }
+            ]
+          }
+        ]
+      })
+    )
+
+    req.end()
+  })
 }
